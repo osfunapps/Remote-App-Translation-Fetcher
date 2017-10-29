8d80c30f3b4be2a265227d0b58dbf37653892895
ofunapps <osfunapps@gmail.com>
2017-10-29 17:30:40 +0200

    added translations in the right location

--- tools/FilesExlorerManager.cs
+++ tools/FilesExlorerManager.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Remotes_App_Translation_Project
 {
     internal class FilesExlorerManager
     {
-        private string FOLDER_TRASNLATIONS = "\\Translations";
+        private string FOLDER_TRASNLATIONS = "Translations";
 
         public Dictionary<string, string> ExtractFilesNamesList()
         {
-            var currentFolder = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + FOLDER_TRASNLATIONS;
+            string currentFolder = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var translationsFolder = currentFolder.Substring(0, currentFolder.IndexOf("bin"))+FOLDER_TRASNLATIONS;
             Dictionary<string, string> languagesDict = new Dictionary<string, string>();
-            string[] files = Directory.GetFiles(currentFolder);
+            string[] files = Directory.GetFiles(translationsFolder);
 
             foreach (var languageFilePath in files)
             {
