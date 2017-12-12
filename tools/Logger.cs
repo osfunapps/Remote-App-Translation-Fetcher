47f219e987870a9929992e49bc21652518ec78cd
osfunapps <osfunapps@gmail.com>
2017-12-12 11:32:51 +0200

    added 4 more languages

--- tools/Logger.cs
+++ tools/Logger.cs
@@ -6,7 +6,11 @@
 
         internal static string GetTxt()
         {
-            return "Version 1.0 - launch";
+            return "Version 1.1" +
+                   "\n- added 8 more languages" +
+                   "\n- splitted output to ac and normal remotes" +
+                   "\n- fixed issues regarding blank lines" +
+                   "\n\nVersion 1.0 - launch";
         }
     }
 }
